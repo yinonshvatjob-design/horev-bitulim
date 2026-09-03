e2a553eb8a553fb026719190c00e5e9997140820
Yinon Shvat <yinonshvatjob@gmail.com>
2026-09-03 18:55:21 +0300

    Add Cache-Control headers and script cache buster v2.5 for desktop browsers

--- server.js
+++ server.js
@@ -11,9 +11,16 @@ const mailer = require('./mailer');
 const app = express();
 const PORT = process.env.PORT || 4050;
 
-// Middlewares
+// Middlewares & Anti-Cache Headers for Desktop Browsers
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
 
 // Serve Static Frontend Assets (Web Client)
 app.use(express.static(__dirname));
